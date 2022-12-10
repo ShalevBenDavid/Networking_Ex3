3cb503772c120053b9bd38257314ca67c2fb7854
Shalev Ben David <105733245+ShalevBenDavid@users.noreply.github.com>
2022-12-11 01:28:39 +0200

    mmkkkk

--- Receiver.c
+++ Receiver.c
@@ -3,35 +3,42 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
+#include <time.h>
 #include <signal.h>
+#include <arpa/inet.h>
 #include <stdbool.h>
 
-#define PORT 8888
-#define MAX_SIZE 300
-#define FILE_SIZE 1048575 // file size + 1 for the \0.
+#define PORT 8084
+#define MAX_CONNECTIONS 300
+#define FILE_SIZE 1048574
+#define ID1 2781
+#define ID2 8413
+#define SEG_SIZE 4096
 
-void recv_file(int);
+
+void recv_message(int);
+double averageTimes = 0.0; // Global variable for average times.
+double times[15]; // Array for holding times.
+int numOfTimes = 0;
 
 int main() {
-    // Creates socket named "socketFD". FD for file descriptor.
+    // Creates endpoint for communication named "socketFD". FD for file descriptor.
     int SocketFD = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Check if we were successful in creating socket.
+    // Check if we were successful in creating socketFD.
     if (SocketFD == -1) {
-        printf("Could not create socket: %d\n", errno);
+        printf("Could not create socket! -> socket() failed with error code: %d\n", errno);
         exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
     }
     else {
-        printf("Socket created successfully!\n");
+        printf("Socket created successfully.\n");
     }
 
     // Check if address is already in use.
     int enableReuse = 1;
-    if (setsockopt(SocketFD, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof enableReuse) ==  -1) {
+    if (setsockopt(SocketFD, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(enableReuse)) <  0) {
         printf("setsockopt() failed with error code: %d\n" , errno);
         exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
     }
@@ -47,17 +54,17 @@ int main() {
 
     // Binding port and address to socket and check if binding was successful.
     if (bind(SocketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
-        printf("bind() failed with error code: %d\n", errno);
+        printf("Failed to bind address && port to socket! -> bind() failed with error code: %d\n", errno);
         close(SocketFD); // close the socket.
         exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
     }
     else {
-        printf("binding was successful!\n");
+        printf("Binding was successful!\n");
     }
 
     // Make server start listening and waiting and check if listen() was successful.
-    if (listen(SocketFD, MAX_SIZE) == -1) { // We allow no more than MAX_SIZE queue connections requests.
-        printf("listen() failed with error code : %d\n", errno);
+    if (listen(SocketFD, MAX_CONNECTIONS) == -1) { // We allow no more than MAX_CONNECTIONS queue connections requests.
+        printf("Failed to start listening! -> listen() failed with error code : %d\n", errno);
         close(SocketFD); // close the socket.
         exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
     }
@@ -67,12 +74,12 @@ int main() {
     struct sockaddr_in clientAddress;
 
     // Accept and establish new TCP connections.
-    while(true) {
+   //while(true) {
         memset(&clientAddress, 0, sizeof(clientAddress));
         unsigned int clientAddressLen = sizeof(clientAddress);
         int clientSocket = accept(SocketFD, (struct sockaddr*)&clientAddress, &clientAddressLen); // Accept connection.
         if (clientSocket == -1) {
-            printf("listen failed with error code: %d\n", errno);
+            printf("Failed to accept connection. -> accept() failed with error code: %d\n", errno);
             close(SocketFD);
             close(clientSocket);
             exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
@@ -80,22 +87,45 @@ int main() {
         else {
             printf("Connection established.\n");
         }
-        write_file(clientSocket);
+
+        //----------------------------------Receive Messages---------------------------------
+        recv_message(clientSocket);
         close(clientSocket);
     }
-}
+//}
 
-// Method for receiving file from sender.
-void recv_file(int clientSocket) {
-    char buffer[FILE_SIZE];
-    while (true) {
-        int n = recv(clientSocket, buffer, FILE_SIZE/2, 0);
-        if (n <= 0) { // If there are no messages or an error occurred, we can stop and return.
-            return;
-        }
-        else {
-            printf("%s", n, buffer);
-        }
-        bzero(buffer,
+// Method for receiving message from sender.
+void recv_message(int clientSocket) {
+    char buffer[FILE_SIZE + 1]; // file size + 1 for the \0.
+    clock_t t;
+    double time = 0.0;
+    size_t recvTotalLength = 0;
+    t = clock();  // Start time.
+    // receive first half
+    while (recvTotalLength <= FILE_SIZE / 2) {
+        bzero(buffer, FILE_SIZE+1);
+        ssize_t receivedBytes = recv(clientSocket, buffer, FILE_SIZE / 2, 0);
+        if (receivedBytes <= 0 || recvTotalLength == FILE_SIZE / 2) break; // break if we got an error (-1) or peer closed half side of the socket (0).
+        printf("%s", buffer);
+        recvTotalLength += receivedBytes;
     }
+    t = clock() - t;  // End time.
+    time = ((double) t) / CLOCKS_PER_SEC; // calculate the elapsed time in seconds.
+    averageTimes += time;
+    numOfTimes++;
+    printf("\n\n\n\n----------------------------\nfinished receiving first half\n------------------------------\n\n\n\n");
+    bzero(buffer, FILE_SIZE+1);
+    recvTotalLength = 0;
+    // receive second half
+    while (recvTotalLength <= FILE_SIZE / 2 - 10) {
+        bzero(buffer, FILE_SIZE+1);
+        ssize_t receivedBytes = recv(clientSocket, buffer, FILE_SIZE / 2, 0);
+//        receivedBytes += recv(clientSocket, buffer, 2, 0);
+        if (receivedBytes <= 0 || recvTotalLength == FILE_SIZE / 2) break; // break if we got an error (-1) or peer closed half side of the socket (0).
+        printf("%s", buffer);
+        recvTotalLength += receivedBytes;
+    }
+    printf("recvTotalLength: %zu\n", recvTotalLength);
+
+    printf("\n----------------------------\nfinished receiving second half\n------------------------------\n");
 }
--- Sender.c
+++ Sender.c
@@ -6,14 +6,26 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <time.h>
 
-#define PORT 8888
+#define PORT 8084
 #define IP_ADDRESS "127.0.0.1"
-#define FILE_SIZE 1048575 // file size + 1 for the \0.
+#define FILE_SIZE 1048574
 #define FILE_NAME "TextFile.txt"
+#define ID1 2781
+#define ID2 8413
 
-int send_message(char*, int);
+// Method for sending message.
+size_t send_message(char message[], int socketFD) {
+    size_t totalLengthSent = 0;
+    while (totalLengthSent < FILE_SIZE/2) {
+        ssize_t bytes = send(socketFD, message + totalLengthSent, FILE_SIZE/2 - totalLengthSent, 0);
+        totalLengthSent += bytes;
+    }
+    return 1;
+}
+
 
 int main() {
     //-------------------------------Read File-----------------------------
@@ -23,32 +35,32 @@ int main() {
 
     // Check if we were successful in opening file.
     if(file_pointer == NULL) {
-        printf("Error in opening file : %d\n", errno);
+        printf("Error in opening file! -> fopen() failed with error code: %d\n", errno);
         exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
     }
     else {
-        printf("File opened successfully!\n");
+        printf("File opened successfully.\n");
     }
 
     // Create array for holding the message.
-    char message[FILE_SIZE] = {0};
+    char message[FILE_SIZE + 1] = {0}; // file size + 1 for the \0.
     fread(message, sizeof(char), FILE_SIZE, file_pointer);
 
     // Closes the stream. All buffers are flushed.
     fclose(file_pointer);
 
-    //-------------------------------Create Socket-----------------------------
+    //-------------------------------Create TCP Connection-----------------------------
 
     // Creates socket named "socketFD". FD for file descriptor.
     int socketFD = socket(AF_INET, SOCK_STREAM, 0);
 
     // Check if we were successful in creating socket.
     if(socketFD == -1) {
-        printf("Could not create socket : %d\n", errno);
+        printf("Could not create socket! -> socket() failed with error code: %d\n", errno);
         exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
     }
     else {
-        printf("Socket created successfully!\n");
+        printf("Socket created successfully.\n");
     }
 
     // Create sockaddr_in for IPv4 for holding ip address and port and clean it.
@@ -61,10 +73,9 @@ int main() {
     serverAddress.sin_addr.s_addr = inet_addr(IP_ADDRESS);
 
     // Convert address to binary.
-    int rval = inet_pton(AF_INET, IP_ADDRESS, &serverAddress.sin_addr);
-    if (rval <= 0)
+    if (inet_pton(AF_INET, IP_ADDRESS, &serverAddress.sin_addr) <= 0)
     {
-        printf("inet_pton() has failed!");
+        printf("Failed to convert IPv4 address to binary! -> inet_pton() failed with error code: %d\n", errno);
         return -1;
     }
 
@@ -73,7 +84,7 @@ int main() {
 
     // Check if we were successful in connecting with server.
     if(connection == -1) {
-        printf("Could not connect to server. connect() failed with error code: %d\n", errno);
+        printf("Could not connect to server! -> connect() failed with error code: %d\n", errno);
         exit(EXIT_FAILURE); // Exit program and print EXIT_FAILURE (defined as 1 in stdlib.h).
     }
     else {
@@ -81,35 +92,32 @@ int main() {
     }
 
     //-------------------------------Send Message---------------------------------
-    char ans = 0;
+    int ans;
     while (true) {
-        printf("Do you want to send file? Enter Y for Yes or N for No.");
-        scanf("%c", ans);
-        if (ans == 'N') { break;}
+        printf("Do you want to send file? Enter Y for Yes or N for No.\n");
+//        ans = (char) getchar();
+        while((ans = getchar()) == '\n' || getchar() == EOF);
+        if (ans == 'N') {break;}
         if (send_message(message, socketFD) == -1) {
-            printf("Failed to send first half of the message!")
+            printf("Failed to send first half of the message!\n");
         }
         else {
-            printf("Sent the first half of the file!")
+            printf("Sent the first half of the message.\n");
         }
+
         if(send_message(message + FILE_SIZE / 2, socketFD) == -1) {
-            printf("Failed to send second half of the message!")
+            printf("Failed to send second half of the message!\n");
         }
         else {
-            printf("Sent the first half of the message");
+            printf("Sent the second half of the message.\n");
         }
     }
+    printf("Exiting...\n");
 
     //-------------------------------Close connection-----------------------------
     close(socketFD);
+    printf("Connection closed!\n");
 
     return 0;
 }
-
-// Method for sending first half of message.
-int send_message(char* message, int socketFD) {
-    return send(socketFD, message, FILE_SIZE/2, 0);
-}
-
-
 
